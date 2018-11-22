c880fc4693b821bc726ec9d5f83412bf2cdff40d
agnes <agnes.ferte@jpl.nasa.gov>
2018-11-21 22:06:10 -0800

    add draft

--- kernels_matching.py
+++ kernels_matching.py
@@ -7,4 +7,13 @@ def redshift_distrib(z,z0,alpha,beta):
     redshift_distrib = z**alpha np.exp(-(z/z0)**beta)
     return redshift_distrib
 
+'''elements of kernel from blended galaxies'''
+def subkernel_blend(R,pz,chi): 
+    subkernel_blend = R*pz
+    return subkernel_blend
+
+
+
+
+
 
